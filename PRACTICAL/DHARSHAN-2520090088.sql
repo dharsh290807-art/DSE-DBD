4af93b1f85cb3c36c392c333071dc1f8f5c6d4a8
dharsh290807-art <dharsh290807@gmail.com>
2026-09-18 14:07:14 +0530

    Delete PRACTICAL/DHARSHAN-2520090088.sql

--- PRACTICAL/DHARSHAN-2520090088.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS bookflow_week1_db;
-USE bookflow_week1_db;
-CREATE TABLE books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    isbn VARCHAR(13) NOT NULL UNIQUE,
-    published_year INT,
-    CONSTRAINT chk_published_year CHECK (published_year < 2027)
-);
